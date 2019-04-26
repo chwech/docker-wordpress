616dc073e39895903b0582ac286944461d89d0a5
chwech <804699297@qq.com>
2019-04-26 11:20:18 +0800

    完善备份数据库

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 startTime=$(date +%Y-%m-%d\ %H:%M:%S)
-logPath="/home/chwech/learn-docker/wordpress/backup.log"
+logPath="/home/chwech/docker/wordpress/backup.log"
 
 # 以追加的方式，把 command 的正确输出结果输出到 file 文件中。
 echo "--${startTime} 开始备份数据库--" >> ${logPath}
@@ -9,8 +9,11 @@ echo "--${startTime} 执行mysql备份命令中...--" >> ${logPath}
 docker-compose exec mysql /var/lib/mysql/backup.sh >> ${logPath}
 # 如果上一个命令的退出状态是0， 即正常执行退出时
 if [ "$?" == 0 ]; then
-cd /home/chwech/docker/wordpress/mysql-data
-tar zcf wordpress.tar.gz wordpress.sql > /dev/null
-rm -f wordpress.sql
+  cd /home/chwech/docker/wordpress/mysql-data
+  tar zcf wordpress.tar.gz wordpress.sql > /dev/null
+  rm -f wordpress.sql
+  echo "-- 备份数据库完成--" >> ${logPath}
+else
+  echo "-- 备份数据库失败--" >> ${logPath}
 fi
-echo "-- 备份数据库完成--" >> ${logPath}
+
